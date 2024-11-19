5014fb19f1de05e57af6f737b1d98ef6ae273fc2
colin-lmr <colin@longleafresearch.com>
2024-11-19 09:26:25 -0500

    Implement coordinate transform

--- indentplot/coordinates.py
+++ indentplot/coordinates.py
@@ -69,17 +69,18 @@ def label_indents(image_path):
     cv2.imshow(window_name, img)
 
     # Prompt for grid dimensions
-    dims = prompt('Enter grid dimensions (# of indents) in form dim1,dim2:')
+    #dims = prompt('Enter grid dimensions (# of indents) in form dim1,dim2:')
 
     # Wait for a key press
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
-    return coord, dims
+    return coord
 
 # Transform physical locations to coincide with image locations (real position -> pixel position
 def transform_grid(coord, results):
     import math
+    import numpy as np
     
     # Function to calculate distance between two points
     def point_distance(pt1, pt2):
@@ -103,14 +104,49 @@ def transform_grid(coord, results):
     pt2 = tuple(results.loc[n2][['Stage X (mm)', 'Stage Z (mm)']])
     phys_distance = point_distance(pt1, pt2)
     
+     ## Rotate grid so second point coincides with its location on image
+    # Calculate angle (in radians) of line between pixel points
+    theta_1 = math.atan((pt1_px[1] - pt2_px[1])/(pt1_px[0] - pt2_px[0]))
+    
+    # Calculate angle (in radians) of line between transform points
+    theta_2 = math.atan((pt1[1] - pt2[1])/(pt1[0] - pt2[0]))
+    
+    # Calculate rotation angle
+    angle = theta_2 - theta_1
+    
+    # Perform rotation of set of coordinates
+    def rotate_grid(x, z, angle):
+        # Store grid coordinates in array
+        grid = np.array([(x,z) for x,z in zip(x,z)])
+    
+        # Perform rotation of matrix using a rotation matrix    
+        rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)],
+                                    [np.sin(angle), np.cos(angle)]])
+        grid_rotated = grid @ rotation_matrix.T
+       
+        # Convert matrix back to lists
+        x_coord = grid_rotated[:,0]
+        z_coord = grid_rotated[:,1]
+        
+        return x_coord, z_coord
+    
+    # Rotate grid
+    x_rotated, z_rotated = rotate_grid(results['Stage X (mm)'], results['Stage Z (mm)'], angle)
+    results['x_transform'] = x_rotated
+    results['z_transform'] = z_rotated
+              
     # Calculate scale factor between pixel and physical distances and scale coordinates
     scale = pixel_distance/phys_distance
-    results['x_transform'] = results['Stage X (mm)'] * scale
-    results['z_transform'] = results['Stage Z (mm)'] * scale
+    results['x_transform'] = results['x_transform'] * scale
+    results['z_transform'] = results['z_transform'] * scale
     
-    # Translate coordinates so that first point coincides with location on image (pixel coordinates)
-    #x_translate = 
-
+    # Translate grid so first point coincides with its location on image - points are now mapped to the image
+    x_translate = coord.loc[n1]['x_pixels'] - results.loc[n1]['x_transform'] 
+    z_translate = coord.loc[n1]['y_pixels'] - results.loc[n1]['z_transform'] 
+    results['x_transform'] = results['x_transform'] + x_translate
+    results['z_transform'] = results['z_transform'] + z_translate
+    
+    return results[['x_transform'], ['z_transform']]
 
 # Interpolate pixel coordinates of all indentations from corner coordinates and number
 # For a grid, need three corner points labeled in the image to determine location and numbers of all points
